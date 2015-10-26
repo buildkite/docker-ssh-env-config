481574a984a3404f31de2534e291ff8af105cdeb
Tim Lucas <t@toolmantim.com>
2015-10-26 15:12:11 +1100

    Add SSH_DEBUG

--- ssh-env-config.sh
+++ ssh-env-config.sh
@@ -6,28 +6,35 @@
 #   SSH_CONFIG - contents of an SSH config file
 #   SSH_KNOWN_HOSTS - contents of an SSH known_hosts file
 #   SSH_PRIVATE_RSA_KEY - contents of an SSH private RSA key
+#   SSH_DEBUG - switch to a high debug level 3 for all hosts, to help solve SSH issues
 #
 # The environment variables are unset after the files are created to help
 # prevent accidental output in logs
 
 set -e
 
+mkdir -p ~/.ssh
+chmod 600 ~/.ssh
+
 [[ ! -z "$SSH_PRIVATE_CONFIG" ]] && \
-  mkdir -p ~/.ssh && \
   echo "$SSH_PRIVATE_CONFIG" > ~/.ssh/config && \
   chmod 600 ~/.ssh/config && \
   unset SSH_PRIVATE_CONFIG
 
 [[ ! -z "$SSH_KNOWN_HOSTS" ]] && \
-  mkdir -p ~/.ssh && \
   echo "$SSH_KNOWN_HOSTS" > ~/.ssh/known_hosts && \
   chmod 600 ~/.ssh/known_hosts && \
   unset SSH_KNOWN_HOSTS
 
 [[ ! -z "$SSH_PRIVATE_RSA_KEY" ]] && \
-  mkdir -p ~/.ssh && \
   echo "$SSH_PRIVATE_RSA_KEY" > ~/.ssh/id_rsa && \
   chmod 600 ~/.ssh/id_rsa && \
   unset SSH_PRIVATE_RSA_KEY
+  
+[[ ! -z "$SSH_DEBUG" ]] && \
+  touch ~/.ssh/config && \
+  chmod 600 ~/.ssh/config && \
+  echo "Host *  \nLogLevel DEBUG3" >> ~/.ssh/config && \
+  unset SSH_DEBUG
 
 [[ $1 ]] && exec "$@"
